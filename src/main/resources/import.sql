e8a7f623b4b3ae85361711935a9b3259f0afc05e
maxzhang <wenyyuan.zhang@concentrix.com>
2021-06-22 15:11:11 +0800

    Add import.sql to insert data when start up

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into user(id, email, first_name, last_name) values(1, 'abc@123.com', 'Bill', 'Clinton');
+insert into user(id, email, first_name, last_name) values(2, 'xyz@123.com', 'Max', 'Zhang');
