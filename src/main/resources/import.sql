0f24d5dc36840015571dd71283dc8024cf60d196
maxzhang <freesky_zh@hotmail.com>
2021-06-22 15:53:01 +0800

    Demo for Hibernate acess MySQL

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,7 @@
-insert into user(id, email, first_name, last_name) values(1, 'abc@123.com', 'Bill', 'Clinton');
-insert into user(id, email, first_name, last_name) values(2, 'xyz@123.com', 'Max', 'Zhang');
+-- H2
+--insert into user(id, email, first_name, last_name) values(1, 'abc@123.com', 'Bill', 'Clinton');
+--insert into user(id, email, first_name, last_name) values(2, 'xyz@123.com', 'Max', 'Zhang');
+
+-- MySQL
+insert into demo.user(id, email, first_name, last_name) values(1, 'abc@123.com', 'Bill', 'Clinton');
+insert into demo.user(id, email, first_name, last_name) values(2, 'xyz@123.com', 'Max', 'Zhang');
